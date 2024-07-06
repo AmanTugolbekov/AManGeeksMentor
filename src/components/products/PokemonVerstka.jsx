d591b95801677d7a38143a8a62c9652fb5084883
Aman <tugolbekovaman3@gmail.com>
2024-07-06 18:01:19 +0600

    first commit

---
+++ src/components/products/PokemonVerstka.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const PokemonVerstka = ({ pokemon }) => {
+  const [elemPokemon, setElemPokemon] = useState();
+  const amin = async () => {
+    const { data } = await axios(pokemon.url);
+    setElemPokemon(data.sprites.front_default);
+    console.log(data);
+  };
+  useEffect(() => {
+    amin();
+  }, []);
+  return (
+    <div
+      className="pokemonCard"
+      style={{
+        display: "flex",
+        backgroundColor: "lightblue",
+        width: "25rem",
+        height: "10rem",
+        borderRadius: "1rem",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "9rem",
+        cursor: "pointer",
+      }}
+    >
+      <img style={{ height: "6rem" }} src={elemPokemon} alt={pokemon.name} />
+      <span style={{ fontWeight: "700", fontSize: "130%" }}>
+        {pokemon.name}
+      </span>
+    </div>
+  );
+};
+
+export default PokemonVerstka;
